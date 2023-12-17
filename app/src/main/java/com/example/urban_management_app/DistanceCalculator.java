3a60799ed383dd21f1243248911ea4223e052185
Ben Johnson <c20316733@mytudublin.ie>
2023-12-17 19:56:15 +0000

    nearest report calculation working (hardcoded origin, no API request)

---
+++ app/src/main/java/com/example/urban_management_app/DistanceCalculator.java
@@ -0,0 +1,54 @@
+package com.example.urban_management_app;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public class DistanceCalculator {
+
+    // check if the origin OR list of reports is empty
+    public static LatLng findNearest(LatLng origin, List<LatLng> points) {
+        if (origin == null || points == null || points.isEmpty()) {
+            return null;
+        }
+
+        // initialise the variables
+        LatLng nearestPoint = null;
+        double nearestDistance = Double.MAX_VALUE;
+
+        // calculate the distance between the user and report, for each report
+        // and store the result
+        for (LatLng point : points) {
+            double distance = calculateDistance(origin, point);
+
+            if (distance < nearestDistance) {
+                nearestDistance = distance;
+                nearestPoint = point;
+            }
+        }
+
+        return nearestPoint;
+    }
+
+    private static double calculateDistance(LatLng point1, LatLng point2) {
+        double lat1 = point1.latitude;
+        double lon1 = point1.longitude;
+        double lat2 = point2.latitude;
+        double lon2 = point2.longitude;
+
+        // haversine formula
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // Earth radius in km
+        double radius = 6371.0;
+
+        return radius * c;
+    }
+}
--- app/src/main/java/com/example/urban_management_app/RouteFinderActivity.java
+++ app/src/main/java/com/example/urban_management_app/RouteFinderActivity.java
@@ -1,21 +1,14 @@
 package com.example.urban_management_app;
 
-import android.Manifest;
-import android.content.Context;
+
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.location.Location;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,112 +16,119 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.maps.DirectionsApi;
-import com.google.maps.DirectionsApiRequest;
-import com.google.maps.GeoApiContext;
-import com.google.maps.android.PolyUtil;
-import com.google.maps.model.DirectionsResult;
-import com.google.maps.model.TravelMode;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class RouteFinderActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private FusedLocationProviderClient fusedLocationClient; // for getting user location
-    private LatLng userLatLng;
-    private List<Report> reportList = new ArrayList<>();
-    private boolean isUserLocationAvailable = false;
-    private boolean areReportsAvailable = false;
-    private Button buttonFindNearest;
+    // for getting user location
+    private FusedLocationProviderClient fusedLocationClient;
 
+    // for origin, destination
+    private LatLng ORIGIN;
+
+    // TODO: hide API key
+    private String key = "AIzaSyC9LPKCQXaX0xMECbk9y-vEPjwgDjxeuUM";
+
+    // google map implementation
+    private GoogleMap mMap;
+    private Polyline currentPolyline; // used for plotting the route on the map
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_finder);
 
-        buttonFindNearest = findViewById(R.id.btnFindReport); // declare the button
+        // TODO: Uncomment after testing
+        //getUserLocation();
+        getHardcodedLocation();
 
-        // create SupportMapFragment and get notified when the map is ready to be used
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        getReportCoordinates();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
 
-        // on click listener for the nearest report button
-        buttonFindNearest.setOnClickListener(new View.OnClickListener() {
+    // map setup
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
 
-            @Override
-            public void onClick(View v) {
-                // get user location x1,y1
-                getUserLocation();
+        // set to Dublin
+        LatLng startingCamera = new LatLng(53.36254986466739, -6.349820583825544);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startingCamera, 12f));
 
-                // get report location x2,y2
-                getReportCoordinates();
+        // apply custom map style
+        try {
+            InputStream inputStream = getResources().openRawResource(R.raw.map_style);
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            String json = new String(buffer, "UTF-8");
+            googleMap.setMapStyle(new MapStyleOptions(json));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-                // call Directions API for (x1,y1) and (x2,y2)
-
-                // display on the map
-
-            }
-        });
+    //TODO: remove for deployment (hardcoded)
+    private void getHardcodedLocation() {
+        ORIGIN = new LatLng(53.35737638828084, -6.35031412259818);
     }
 
     // function for getting the current user location
     private void getUserLocation() {
-        // check if user has allowed location check
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, location -> {
-                        if (location != null) { //TODO: remove current location toast
+                        if (location != null) {
                             Toast.makeText(RouteFinderActivity.this, "Current Location: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_LONG).show();
-                            userLatLng = new LatLng(location.getLatitude(), location.getLongitude()); // store user coordinates
+                            ORIGIN = new LatLng(location.getLatitude(), location.getLongitude());
                         } else {
-                            Toast.makeText(RouteFinderActivity.this, "Location not available. Allow location services in order to proceed.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RouteFinderActivity.this, "Location not available. Allow location services to proceed.", Toast.LENGTH_SHORT).show();
                         }
                     });
         } else {
-            // request location permission
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
     }
 
-    // currently cycles through all reports, only need the nearest
-    private List<LatLng> getReportCoordinates() {
-        List<LatLng> coordinates = new ArrayList<>();
-
+    // function to get the nearest report and plot the route
+    private void getReportCoordinates() {
         Query reportsQuery = FirebaseDatabase.getInstance().getReference("reports");
 
         reportsQuery.addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                coordinates.clear(); // assuming coordinates is a List<LatLng>
+                // list to store report coordinates
+                List<LatLng> coordinates = new ArrayList<>();
 
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     Report report = snapshot.getValue(Report.class);
 
-                    if (report != null) { //TODO: check here for the nearest report to user
+                    if (report != null) {
+                        // extract coordinates and add to LatLng List
                         LatLng reportLatLng = new LatLng(report.getXCoordinates(), report.getYCoordinates());
-                        Toast.makeText(RouteFinderActivity.this, "Report Location: " + reportLatLng.latitude + ", " + reportLatLng.longitude, Toast.LENGTH_SHORT).show();
                         coordinates.add(reportLatLng);
-                    } else {
-                        Toast.makeText(RouteFinderActivity.this, "No coordinates in report.", Toast.LENGTH_LONG).show();
                     }
                 }
+
+                List<LatLng> points = coordinates;
+
+                LatLng nearestPoint = DistanceCalculator.findNearest(ORIGIN, points);
+                System.out.println("Nearest Point: " + nearestPoint);
             }
 
             @Override
@@ -136,27 +136,14 @@ public class RouteFinderActivity extends FragmentActivity implements OnMapReadyC
                 // TODO: Handle this
             }
         });
-
-        return coordinates;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        LatLng currentLocation = new LatLng(53.350140, -6.266155);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 12f));
-        mMap.setMyLocationEnabled(true); // uncomment to show user location on the map, san jose USA
-    }
+    // TODO: get Directions
+
+    // TODO: make API call
+
+    // TODO: plot on map
+
 }
 
 
